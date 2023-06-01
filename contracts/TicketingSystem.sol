19f9403068501e29e57ffe0bfd132e3cc892a4ee
Abda Rafi <abda_rafi@yahoo.com>
2023-06-01 01:31:12 +0000

    add ticketing contract

---
+++ contracts/TicketingSystem.sol
@@ -0,0 +1,50 @@
+pragma solidity >=0.4.25 <0.9.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+contract TicketingSystem is ERC721, Ownable {
+    uint256 private nextTicketId;
+
+    struct Ticket {
+        string eventName;
+        uint256 eventDate;
+        uint256 eventTime;
+        uint256 seatNumber;
+    }
+
+    mapping(uint256 => Ticket) public tickets;
+
+    constructor() ERC721("TicketingSystem", "TS") {
+        nextTicketId = 1;
+    }
+
+    function mintTicket(
+        address to,
+        string memory eventName,
+        uint256 eventDate,
+        uint256 eventTime,
+        uint256 seatNumber
+    ) public onlyOwner {
+        Ticket memory newTicket = Ticket(eventName, eventDate, eventTime, seatNumber);
+        tickets[nextTicketId] = newTicket;
+        _mint(to, nextTicketId);
+        nextTicketId++;
+    }
+
+    function buyTicket(uint256 ticketId) public payable {
+        require(msg.value >= getTicketPrice(ticketId), "Insufficient funds");
+        address currentOwner = ownerOf(ticketId);
+        _transfer(currentOwner, msg.sender, ticketId);
+        payable(currentOwner).transfer(msg.value);
+    }
+
+    function getTicketPrice(uint256 ticketId) public pure returns (uint256) {
+        // Replace this function with a more sophisticated pricing mechanism if desired
+        return 0.1 ether;
+    }
+
+    function verifyTicketOwnership(uint256 ticketId, address claimant) public view returns (bool) {
+        return ownerOf(ticketId) == claimant;
+    }
+}
