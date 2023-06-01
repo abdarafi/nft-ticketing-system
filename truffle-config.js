5545af0ccd96359f188131247e21bf2724ca38e2
Abda Rafi <abda_rafi@yahoo.com>
2023-06-01 01:31:51 +0000

    adjust dependencies

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   networks: {
     development: {
-     host: "127.0.0.1",
-     port: 8545,
-     network_id: "*"
+     host: "10.0.2.2",
+     port: 7545,
+     network_id: "*",
+     private_keys: ["0xf5dc35842b95c7df562c10e804bc109e91cabff2f36f294ef1f9e8945e1d01e6"]
     },
     dashboard: {
     }
