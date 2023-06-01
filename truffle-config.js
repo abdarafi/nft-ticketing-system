57edbe20004496c64b2e18375f81522d811d530f
Abda Rafi <abda_rafi@yahoo.com>
2023-06-01 01:32:29 +0000

    add comment

--- truffle-config.js
+++ truffle-config.js
@@ -4,7 +4,7 @@ module.exports = {
      host: "10.0.2.2",
      port: 7545,
      network_id: "*",
-     private_keys: ["0xf5dc35842b95c7df562c10e804bc109e91cabff2f36f294ef1f9e8945e1d01e6"]
+     private_keys: ["0xf5dc35842b95c7df562c10e804bc109e91cabff2f36f294ef1f9e8945e1d01e6"] //not a real private key
     },
     dashboard: {
     }
